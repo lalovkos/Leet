98cab05f7135dd2c9b73c41927dcc29d6ef1ad74
Константин Лалов <lalovkos@gmail.com>
2022-04-08 18:19:03 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RomanToInteger.RomanToInt("III"));
+        }
+    }
+}
---
+++ RomanToInteger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Leet
+{
+    public class RomanToInteger
+    {
+        public static int RomanToInt(string s)
+        {
+            Dictionary<char, int> romanNumbers = new Dictionary<char, int>
+            {
+                { 'M', 1000},
+                { 'D', 500}, 
+                { 'C', 100},
+                { 'L', 50}, 
+                { 'X', 10}, 
+                { 'V', 5}, 
+                { 'I', 1}
+            };
+
+            int sum = 0;
+            var curNumberIndex = romanNumbers.GetEnumerator();
+
+            //Main cycle for string
+            foreach (char curChar in s)
+            {
+                char curNumber = curNumberIndex.Current.Key;
+                if (romanNumbers[curNumber] < romanNumbers[curChar])
+                {
+                    curNumberIndex.MoveNext();
+                }
+                else
+                {
+                    
+                }
+
+                Console.WriteLine(curChar);
+            }
+
+            return sum;
+        }
+    }
+}
